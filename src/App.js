34470bd70272c2b3a8d3b35f75b062f117504c40
rajm688 <rajkumarm688@gmail.com>
2021-12-31 15:59:15 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { useState } from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+export default function App() {
+  const [state,setstate]= useState(" ")
+  const[list,setlist] = useState(["Create theme","Work on wordpress", "Organize office main department","Error solve in HTML template"])
+  console.log(state)
+  console.log(list)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card sx={{ width:"80%", padding:"10px"}}>
+        <div>
+      <TextField style={{width:"60%",margin:"auto"}} onChange={(e)=>setstate(e.target.value)} id="outlined-basic" label="Outlined" variant="outlined" />
+      <Button  style={{heigth:"100%",width:"30%",margin:"auto"}} onClick={()=>setlist([...list,state])}variant="contained">ADD</Button>
+      </div>
+  {list.map(item=><Tasks item={item}/>)}
+  </Card>
     </div>
   );
 }
 
-export default App;
+function Tasks({item}){
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  return(
+    <div>
+      <Card sx={{ width:"90%", padding:"10px", margin:"auto" }}>
+      <Checkbox {...label} />{item}
+  </Card>
+
+    </div>
+  )
+}
